0b83e7796acc1b8c431f6427d07fd0a7ad90012c
kongweiteng <kongweiteng@enn.cn>
2019-07-08 09:44:19 +0800

    juc

--- basics/src/main/java/com/kongweiteng/basics/jucTest.java
+++ basics/src/main/java/com/kongweiteng/basics/jucTest.java
@@ -1,23 +1,43 @@
 package com.kongweiteng.basics;
 
-import java.util.UUID;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 public class jucTest {
 
     public static void main(String[] args) {
 
-        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+
+
+
+
+//        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
 //        List<String> list = Collections.synchronizedList(new ArrayList<>());
 //        List<String> list = new Vector<>();
-        for (int i = 0; i < 10000; i++) {
-            new Thread(() -> {
-                list.add(UUID.randomUUID().toString().substring(0, 4));
-                System.err.println(list.size());
-            }, String.valueOf(i)).start();
+//        Set<String> set = new HashSet<>();
+//        HashMap<String, String> stringStringHashMap = new HashMap<>();
+//
+//        String put = stringStringHashMap.put("75643", "5447869");
+//        String put1 = stringStringHashMap.put("75643", "978654876");
+//        String s = stringStringHashMap.get("");
+//        System.err.println(put1);
+//        System.err.println(stringStringHashMap);
+//
 
-        }
+//        Hashtable<String, String> stringStringHashtable = new Hashtable<>();
+//        String put = stringStringHashtable.put("6543", "654365");
+//        String put1 = stringStringHashtable.put("6543", "87645463765");
+//        System.err.println(put1);
+//        System.err.println(stringStringHashtable);
 
-
+//        for (int i = 0; i < 100000; i++) {
+//            new Thread(() -> {
+//                set.add(UUID.randomUUID().toString().substring(0, 4));
+//                System.err.println(set.size());
+//            }, String.valueOf(i)).start();
+//
+//        }
     }
+
+
+
+
+
 }
